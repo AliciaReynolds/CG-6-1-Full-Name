ae0b6b7e72116f68dee7a9b6f239012c779e961a
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-16 22:31:51 -0500

    First writing the code

--- CG 6-1 Full Name/Program.cs
+++ CG 6-1 Full Name/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 
 namespace CG_6_1_Full_Name
 {
@@ -6,7 +8,27 @@ namespace CG_6_1_Full_Name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please provide your first name.");
+            string First = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name.");
+            string Last = Console.ReadLine();
+
+            StringBuilder FullName = new StringBuilder();
+            FullName.Append(First);
+            FullName.Append(Last);
+
+            Console.WriteLine(FullName);
+
+            Console.ReadLine();
+
+
         }
+
+        //private static string Name(string FirstName, string LastName);
+        //{
+        //return $"Hello {FristName}{LastName}";
+        //}
+
     }
 }
