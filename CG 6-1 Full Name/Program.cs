b6e2fe4833a57d26a90e68d1da78bcf4fcaa2c14
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-16 22:40:43 -0500

    trying to do a separate method and failing

--- CG 6-1 Full Name/Program.cs
+++ CG 6-1 Full Name/Program.cs
@@ -8,27 +8,42 @@ namespace CG_6_1_Full_Name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please provide your first name.");
-            string First = Console.ReadLine();
+            //Console.WriteLine("Please provide your first name.");
+            //string First = Console.ReadLine();
 
-            Console.WriteLine("Please enter your last name.");
-            string Last = Console.ReadLine();
+            //Console.WriteLine("Please enter your last name.");
+            //string Last = Console.ReadLine();
 
-            StringBuilder FullName = new StringBuilder();
-            FullName.Append(First);
-            FullName.Append(Last);
+            //StringBuilder FullName = new StringBuilder();
+            //FullName.Append(First);
+            //FullName.Append(" ");
+            //FullName.Append(Last);
 
-            Console.WriteLine(FullName);
+            //Console.WriteLine(FullName);
+
 
             Console.ReadLine();
 
 
         }
 
-        //private static string Name(string FirstName, string LastName);
-        //{
-        //return $"Hello {FristName}{LastName}";
-        //}
+        private static void Name();
+        {
+        
+            Console.WriteLine("Please provide your first name.");
+            string First = Console.ReadLine();
 
-    }
+        Console.WriteLine("Please enter your last name.");
+            string Last = Console.ReadLine();
+
+        StringBuilder FullName = new StringBuilder();
+        FullName.Append(First);
+            FullName.Append(" ");
+            FullName.Append(Last);
+
+            return Console.WriteLine(FullName);
+
+        }
+
+}
 }
